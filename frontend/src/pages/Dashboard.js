48f7e32ddcee5ae3799bb94daf1c61464cc835f8
stefanspiridon <bd.stefanspiridon@gmail.com>
2021-12-29 11:48:55 +0000

    added basic tradingview widget

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -2,12 +2,15 @@ import React from 'react'
 import Navbar from "../components/Navbar";
 import jwt from 'jsonwebtoken'
 import { useHistory } from 'react-router-dom'
+import TradingViewWidget from 'react-tradingview-widget';
 
 function Dashboard() {
     return (
         <div className='dashboard'>
             <Navbar />
-            <h1>Dashboard</h1> 
+            <h1>Dashboard</h1>
+            <TradingViewWidget symbol="NASDAQ:AAPL" />
+            
         </div>
     )
 }
